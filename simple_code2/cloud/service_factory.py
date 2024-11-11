f1e1ba024002d4d711be76fcb52a3533f8fb7266
al.ma <al.ma@itstar.tw>
2024-11-11 10:37:34 +0800

    style: 註解

--- simple_code2/cloud/service_factory.py
+++ simple_code2/cloud/service_factory.py
@@ -18,10 +18,15 @@ class CloudServiceFactory:
 
         此工厂方法负责生成不同类型的云服务实例，以便调用方通过统一的接口（CloudServiceInterface）
         来与具体的云服务进行交互。具体的实例类型取决于传入的服务提供者的具体类型。
+        Args:
+            service_provider: ServiceProvider 实例，用于指示需要创建的云服务类型
 
-        :param service_provider: ServiceProvider 实例，用于指示需要创建的云服务类型
-        :return: CloudServiceInterface 实例，表示指定的云服务接口实现类
-        :raises ValueError: 如果 service_provider 类型不受支持，可能抛出此异常
+        Returns:
+            CloudServiceInterface 实例，表示指定的云服务接口实现类
+
+        Raises:
+            ValueError: 如果 service_provider 类型不受支持，可能抛出此异常
+
         """
         if service_provider == ServiceProvider.ALI:
             return AliService("Ali Cloud Service")
